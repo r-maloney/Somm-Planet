9594ff39811877a39f0c51de96e8d19eb1b3124a
Ryan <rdmaloney3@gmail.com>
2021-01-22 16:16:58 -0500

    setup server and test route

--- backend/app.js
+++ backend/app.js
@@ -6,4 +6,39 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 
 const { environment } = require("./config");
+const routes = require("./routes");
+
 const isProduction = environment === "production";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(routes);
+
+module.exports = app;
