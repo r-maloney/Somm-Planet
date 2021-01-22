51c8ab1c39c36c34b9086632e61544d6348cdc44
Ryan <rdmaloney3@gmail.com>
2021-01-22 15:55:43 -0500

    setup middleware for express backend

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
