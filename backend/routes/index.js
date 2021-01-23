7a0783c49247af0e3c4045cc2244b19f789a72dd
Ryan <rdmaloney3@gmail.com>
2021-01-23 17:48:04 -0500

    added validator middleware for registration

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,4 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
-
 module.exports = router;
