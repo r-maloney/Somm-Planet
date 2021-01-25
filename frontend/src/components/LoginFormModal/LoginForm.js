e584a11bb7a974ca097cf5afa34c8a6ba9c1f637
Ryan <rdmaloney3@gmail.com>
2021-01-25 16:41:52 -0500

    finished styling for login modal

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -21,15 +21,15 @@ function LoginForm() {
   };
 
   return (
-    <>
+    <div className='login__container'>
       <Link to='/login' className='login-signup__tab--active'>
         Log In
       </Link>
       <Link to='/signup' className='login-signup__tab--inactive'>
         Sign Up
       </Link>
-      <form onSubmit={handleSubmit}>
-        <div className='login__form'>
+      <form onSubmit={handleSubmit} className='login__form'>
+        <div>
           <ul className='login__errors-list'>
             {errors.map((error, idx) => (
               <li className='login__error' key={idx}>
@@ -38,30 +38,24 @@ function LoginForm() {
             ))}
           </ul>
           <div className='login__form-group'>
-            <label className='login__label'>
-              Login
-              <input
-                className='login__input'
-                type='text'
-                value={credential}
-                placeholder='Email or Username'
-                onChange={(e) => setCredential(e.target.value)}
-                required
-              />
-            </label>
+            <label className='login__label'>Email or Username</label>
+            <input
+              className='login__input'
+              type='text'
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
           </div>
           <div className='login__form-group'>
-            <label className='login__label'>
-              Password
-              <input
-                placeholder='Password'
-                className='login__input'
-                type='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </label>
+            <label className='login__label'>Password</label>
+            <input
+              className='login__input'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <button className='login__button' type='submit'>
             Log In
@@ -69,7 +63,7 @@ function LoginForm() {
           {/* <a href="/demo?_csrf=csrf" id="auth-splash__form-demo">Login as demo user</a>  */}
         </div>
       </form>
-    </>
+    </div>
   );
 }
 
