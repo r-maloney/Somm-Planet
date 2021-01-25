30aba7a1105cdf84a6e14bad8d28d26032f7081a
Ryan <rdmaloney3@gmail.com>
2021-01-24 20:59:42 -0500

    created restoreUser thunk to keep a user logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-      </Route>
-    </Switch>
+    isLoaded && (
+      <Switch>
+        <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
