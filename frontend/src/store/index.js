6a34446e3f676832ea19f4a1d3c1629972b27cb8
Ryan <rdmaloney3@gmail.com>
2021-01-24 20:50:51 -0500

    created login component

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session: sessionReducer,
 });
 
 let enhancer;
