37bb360ebc5c51f2dfa3f2c9c8d889c3c5dedbcf
Ryan <rdmaloney3@gmail.com>
2021-01-25 16:02:11 -0500

    created modal popout for signin

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import "./LoginForm.css";
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -19,32 +21,55 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type='text'
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type='submit'>Log In</button>
-    </form>
+    <>
+      <Link to='/login' className='login-signup__tab--active'>
+        Log In
+      </Link>
+      <Link to='/signup' className='login-signup__tab--inactive'>
+        Sign Up
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <div className='login__form'>
+          <ul className='login__errors-list'>
+            {errors.map((error, idx) => (
+              <li className='login__error' key={idx}>
+                {error}
+              </li>
+            ))}
+          </ul>
+          <div className='login__form-group'>
+            <label className='login__label'>
+              Login
+              <input
+                className='login__input'
+                type='text'
+                value={credential}
+                placeholder='Email or Username'
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div className='login__form-group'>
+            <label className='login__label'>
+              Password
+              <input
+                placeholder='Password'
+                className='login__input'
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <button className='login__button' type='submit'>
+            Log In
+          </button>
+          {/* <a href="/demo?_csrf=csrf" id="auth-splash__form-demo">Login as demo user</a>  */}
+        </div>
+      </form>
+    </>
   );
 }
 
